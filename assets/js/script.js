6246278a750b19f093ff6acf34a761803e98414d
MattVogelsang <Vogelsangmatt@gmail.com>
2024-09-05 12:56:00 -0400

    updated JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,12 @@ let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    if(nextId){
+        nextId = nextId 
+    }else {
+        nextId = 1
+    }
+    localStorage.setItem("nextId", JSON.stringify(nextId))
 }
 
 // Todo: create a function to create a task card
@@ -13,7 +18,7 @@ function createTaskCard(task) {
     const taskCard = $('<div>')
       .addClass('card task-card draggable my-3')
       .attr('data-task-id', task.id);
-    const cardHeader = $('<div>').addClass('card-header h4').text(task.name);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.title);
     const cardBody = $('<div>').addClass('card-body');
     const cardDescription = $('<p>').addClass('card-text').text(task.type);
     const cardDueDate = $('<p>').addClass('card-text').text(task.dueDate);
@@ -21,7 +26,7 @@ function createTaskCard(task) {
       .addClass('btn btn-danger delete')
       .text('Delete')
       .attr('data-task-id', task.id);
-    cardDeleteBtn.on('click', handleDeletetask);
+    cardDeleteBtn.on('click', handleDeleteTask);
   
     // ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
     if (task.dueDate && task.status !== 'done') {
@@ -45,10 +50,49 @@ function createTaskCard(task) {
     return taskCard;
 }
 // Todo: create a function to render the task list and make cards draggable
+
+
 function renderTaskList() {
-
-}
-
+   
+  
+    // ? Empty existing project cards out of the lanes
+    const todoList = $('#todo-cards');
+    todoList.empty();
+  
+    const inProgressList = $('#in-progress-cards');
+    inProgressList.empty();
+  
+    const doneList = $('#done-cards');
+    doneList.empty();
+  
+    // ? Loop through projects and create project cards for each status
+    for (let project of taskList) {
+      if (project.status === 'to-do') {
+        todoList.append(createProjectCard(project));
+      } else if (project.status === 'in-progress') {
+        inProgressList.append(createProjectCard(project));
+      } else if (project.status === 'done') {
+        doneList.append(createProjectCard(project));
+      }
+    }
+  
+    // ? Use JQuery UI to make task cards draggable
+    $('.draggable').draggable({
+      opacity: 0.7,
+      zIndex: 100,
+      // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+      helper: function (e) {
+        // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+        const original = $(e.target).hasClass('ui-draggable')
+          ? $(e.target)
+          : $(e.target).closest('.ui-draggable');
+        // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+        return original.clone().css({
+          width: original.outerWidth(),
+        });
+      },
+    });
+  }
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
 
